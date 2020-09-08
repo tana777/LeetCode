d6890622c9b20810e9ab1d6f3080209297239c65
Tana Gegen <istana.gegen@gmail.com>
2020-09-07 23:05:30 -0700

    updates

---
+++ Divide_Conquer/153_Find_Mimimum_in_Rotated_Sorted_Array.py
@@ -0,0 +1,74 @@
+""" Given an array of size n, find the majority element. 
+
+The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array. """ 
+
+
+# Method 1 sort
+"""
+    Time Complexity: O(nlgn)
+    Space Complexity: O(1)
+"""
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        nums.sort()
+        return nums[0]
+
+
+
+# Method 2 iterate over the list, if prev value is greater than the current, then we find the minimum
+# due to the property of rotated and sorted array
+"""
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+"""
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        for i in range(len(nums)):
+            if nums[i-1] > nums[i]:
+                return nums[i]
+        return nums[0]
+
+
+# Method 3 Divide and Conquer
+
+"""
+Idea: Divide and conquer.
+
+Evenly Split the array into two sub-arrays, and find the minimums of them, return the smaller one.
+findMin(a[0..n]) = min(findMin(a[0..n/2], a[n/2..n])
+
+Key property:
+One of the sub-array will be a sorted array, it takes O(1) to find the minimal element, just the first element.
+"""
+
+"""
+    Time Complexity: T(n) = O(1) + T(n/2) = O(logn)
+    Space Complexity: O(logn)
+"""
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if self.isSorted(nums):
+            return nums[0]
+        middle = len(nums) // 2
+        return min(self.findMin(nums[:middle]), self.findMin(nums[middle:]))
+    
+    def isSorted(self, nums):
+        return nums[0] <= nums[-1]
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        return self.minHelper(nums, l = 0, r = len(nums)-1)
+    
+    def minHelper(self, nums, l, r):
+        if l==r:
+            return nums[l]
+        if nums[l] < nums[r]:
+            return nums[l]
+        middle = l + (r-l)//2
+        return min(self.minHelper(nums, l, middle), self.minHelper(nums, middle+1, r))
+        
---
+++ Divide_Conquer/912_Sort_an_Array.py
@@ -0,0 +1,230 @@
+"""
+    Given an array of integers nums, sort the array in ascending order.
+
+Example 1:
+Input: nums = [5,2,3,1]
+Output: [1,2,3,5]
+
+Example 2:
+Input: nums = [5,1,1,2,0,0]
+Output: [0,0,1,1,2,5]
+"""
+
+
+## Method 1 QuickSort - partition
+"""
+Time complexity: O(nlogn) ~ O(n^2)
+Space complexity: O(logn) ~ O(n)
+"""
+
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        self.quickSort(nums, 0, len(nums)-1)
+        return nums
+    
+    def quickSort(self, nums, start, end):
+        if start >= end:
+            return
+        pivot = nums[start]
+        
+        l, r = start, end
+        while r >= l:
+            while r >= l and nums[l] < pivot:
+                l += 1
+            while r >= l and nums[r] > pivot:
+                r -= 1
+            if r >= l:
+                nums[r], nums[l] = nums[l], nums[r]
+                l += 1
+                r -= 1
+                
+        self.quickSort(nums, start, r)
+        self.quickSort(nums, l, end)
+
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        self.quickSort(nums, 0, len(nums)-1)
+        return nums
+    
+    def quickSort(self, nums, start, end):
+        if start >= end:
+            return
+        l, r = self.partition(nums, start, end)
+        self.quickSort(nums, start, r)
+        self.quickSort(nums, l, end)
+        
+    def partition(self, nums, start, end):
+        pivot = nums[start] # start, end, random.choice won't affect the correctness of the code
+        l, r = start, end
+        while r >= l:
+            while r >= l and nums[l] < pivot:
+                l += 1
+            while r >= l and nums[r] > pivot:
+                r -= 1
+            if r >= l:
+                nums[r], nums[l] = nums[l], nums[r]
+                l += 1
+                r -= 1
+        return l, r
+
+
+# Method 2 MergeSort
+"""
+Time complexity: O(nlogn)
+Space complexity: O(logn + n)
+"""
+# First Version
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        return self.mergeSort(nums)
+
+    def mergeSort(self,nums):
+        if len(nums) > 1:
+            m = len(nums) // 2
+            l = self.mergeSort(nums[:m])
+            r = self.mergeSort(nums[m:])
+            return merge(l,r)
+        return nums
+    
+    def merge(self, list1, list2):
+        if not list1 and not list2:
+            return list1
+        if not list1 or not list2:
+            return list1 + list2
+        res = []
+        i = j = 0
+        while len(res) < len(list1) + len(list2):
+            if list1[i] <= list2[j]:
+                res.append(list1[i])
+                if i < len(list1)-1:
+                    i += 1  
+                else:
+                    res.extend(list2[j:])
+                    break
+            else:
+                res.append(list2[j])
+                if j < len(list2)-1:
+                    j += 1
+                else:
+                    res.extend(list1[i:])
+                    break
+        return res
+        
+# Improvement
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums) == 1:
+            return nums
+        return self.mergeSort(nums)
+        
+    def mergeSort(self,nums):
+        if len(nums) > 1:
+            m = len(nums) // 2
+            l = self.mergeSort(nums[:m])
+            r = self.mergeSort(nums[m:])
+            return self.merge(l, r)
+        return nums
+        
+    def merge(self, list1, list2):
+        sorted_list = []
+        while list1 and list2:
+            if list1[0] <= list2[0]:
+                sorted_list.append(list1.pop(0))
+            else:
+                sorted_list.append(list2.pop(0))
+        if list1:
+            sorted_list.extend(list1)
+        if list2:
+            sorted_list.extend(list2)
+            
+        return sorted_list
+            
+            
+            
+# Method 3 Selection sort -- Time Limit Exceeded
+"""
+    Time Complexity: O(n^2)
+    Space Complexity: O(1)
+
+""" 
+"""
+arr[] = 64 25 12 22 11
+
+// Find the minimum element in arr[0...4]
+// and place it at beginning
+11 25 12 22 64
+
+// Find the minimum element in arr[1...4]
+// and place it at beginning of arr[1...4]
+11 12 25 22 64
+
+// Find the minimum element in arr[2...4]
+// and place it at beginning of arr[2...4]
+11 12 22 25 64
+
+// Find the minimum element in arr[3...4]
+// and place it at beginning of arr[3...4]
+11 12 22 25 64 
+"""
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            minVal = min(nums[i:]) # O(n)
+            minIdx = nums[i:].index(minVal) + i
+            nums[i], nums[minIdx] = nums[minIdx], nums[i]
+        return nums
+
+            
+# Method 4 Heapsort
+## Heapify procedure can be applied to a node only if its children nodes are heapified. 
+# So the heapification must be performed in the bottom-up order.
+"""
+    Time Complexity: O(nlogn)
+    Space Complexity: O(1)
+"""
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        # create a max heap
+        for i in range(n//2)[::-1]:
+            self.heapify(nums, n, i)
+            
+        # maintain the maxheap and extact the maximum
+        for i in range(n)[::-1]:
+            nums[0], nums[i] = nums[i], nums[0]
+            self.heapify(nums, i, 0)
+        return nums
+    
+    def heapify(self, nums, n, i):
+        l = 2 * i + 1
+        r = 2 * i + 2
+        largest = i
+        
+        if l < n and nums[l] > nums[largest]:
+            largest = l
+            
+        if r < n and nums[r] > nums[largest]:
+            largest = r
+            
+        if largest != i:
+            nums[i], nums[largest] = nums[largest], nums[i]
+            
+            self.heapify(nums, n, largest)
+    
+            
+            
+# Method 5 Bubble sort 
+# Time Limit Exceeded
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            # last i elements already in place
+            for j in range(n-1-i):
+                if nums[j] > nums[j+1]:
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
+        return nums
+
