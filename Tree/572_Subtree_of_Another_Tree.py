94e79fecf6ce465b1ca0c4144522a2fc81e29eeb
Tana Gegen <istana.gegen@gmail.com>
2020-10-04 19:37:17 -0700

    complete subtree of another tree

---
+++ Tree/572_Subtree_of_Another_Tree.py
@@ -0,0 +1,68 @@
+"""
+Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node values with a subtree of s. A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s could also be considered as a subtree of itself.
+
+Example 1:
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+Given tree t:
+   4 
+  / \
+ 1   2
+Return true, because t has the same structure and node values with a subtree of s.
+ 
+
+Example 2:
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+    /
+   0
+Given tree t:
+   4
+  / \
+ 1   2
+Return false.
+ 
+"""
+
+# Approach: DFS + check isSametree
+"""
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        if not t:
+            return True
+        if not s:
+            return False
+        if s.val == t.val and self.isSametree(s, t):
+            return True
+        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+        
+        
+    def isSametree(self, p, q):
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSametree(p.left, q.left) and self.isSametree(p.right, q.right)
+        
+        
