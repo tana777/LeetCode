ec1bdad5843064b316b6e85e932b053952b283c3
Tana Gegen <istana.gegen@gmail.com>
2020-10-11 18:29:56 -0700

    update

--- Search/46_Permutations.py
+++ Search/46_Permutations.py
@@ -15,27 +15,36 @@ Output:
 ]
 """
 # Approach: DFS + backtracking -- Permutation
+"""
+Time Complexity: O(n*n!) -- n! permutation results, for each permutation, we use O(n) to get it
+Space Complexity: O(n*n!)
+If we are not returning an array here so linear space because our recursion will go at maximum n elements deep 
+since we make n choices of placement at maximum
+"""
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        results = []
-        permutation = []
         n = len(nums)
-        used = [False]*n
-        self.toFindAllPermutations(results, nums, permutation, used, n)
-        return results
+        res = []
+        permutation = []
+        visited = set()
+        self.backtrack(nums,n,res,permutation,visited)
+        return res
     
-    def toFindAllPermutations(self, results, nums, permutation, used, n):
+    def backtrack(self, nums, n, res, permutation, visited):
         if len(permutation) == n:
-            results.append(permutation[:])
+            res.append(permutation[:])
             return
         
         for i in range(n):
-            if used[i]:
-                continue
-            used[i] = True
-            permutation.append(nums[i])
-            self.toFindAllPermutations(results, nums, permutation, used, n)
-            permutation.pop()
-            used[i] = False
+            if i not in visited:
+                visited.add(i)
+                permutation.append(nums[i])
+                self.backtrack(nums,n,res,permutation,visited)
+                permutation.pop()
+                visited.remove(i)
+
+                
+        
+        
         
         
